5b49c13a5d70706a49cba9140114450b2b380f90
rawsta <rawsta@rawsta.de>
2021-06-26 19:11:55 +0200

    addaed some page-templates

---
+++ archive.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Raw Child.
+ *
+ * structuring the posts and pages
+ *
+ * @package Raw Child
+ * @author  rawsta
+ * @license GPL-2.0-or-later
+ *  @link    https://github.com/rawsta/raw-child/
+ */
+
+// Full Width
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+
+/**
+ * Blog Archive Body Class
+ *
+ */
+function raw_child_blog_archive_body_class( $classes ) {
+	$classes[] = 'archive';
+	return $classes;
+}
+add_filter( 'body_class', 'raw_child_blog_archive_body_class' );
+
+// Move breadcrumbs
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+add_action( 'genesis_archive_title_descriptions', 'genesis_do_breadcrumbs', 8 );
+
+// Remove description on paginated archives
+if( get_query_var( 'paged' ) ) {
+	remove_action( 'genesis_archive_title_descriptions', 'genesis_do_archive_headings_intro_text', 12, 3 );
+}
+
+genesis();
---
+++ single.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Raw Child.
+ *
+ * single.php
+ *
+ * @package Raw Child
+ * @author  rawsta
+ * @license GPL-2.0-or-later
+ *  @link    https://github.com/rawsta/raw-child/
+ */
+
+// Entry category in header
+add_action( 'genesis_entry_header', 'raw_child_entry_category', 8 );
+add_action( 'genesis_entry_header', 'raw_child_entry_author', 12 );
+add_action( 'genesis_entry_header', 'raw_child_entry_header_share', 13 );
+
+/**
+ * Entry header share
+ *
+ */
+function raw_child_entry_header_share() {
+	do_action( 'raw_child_entry_header_share' );
+}
+
+/**
+ * After Entry
+ *
+ */
+function raw_child_single_after_entry() {
+	echo '<div class="after-entry">';
+
+	// Breadcrumbs
+	genesis_do_breadcrumbs();
+
+	// Publish date
+	echo '<p class="publish-date">Published on ' . get_the_date( 'F j, Y' ) . '</p>';
+
+	// Sharing
+	do_action( 'raw_child_entry_footer_share' );
+
+	// Author Box
+	genesis_do_author_box_single();
+}
+add_action( 'genesis_after_entry', 'raw_child_single_after_entry', 8 );
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+remove_action( 'genesis_after_entry', 'genesis_do_author_box_single', 8 );
+
+genesis();
